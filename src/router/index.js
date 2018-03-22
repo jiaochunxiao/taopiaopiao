defb90ea0e524f6b38475487394b8aba4bc33e8f
jiaochunxiao <jcx13627607951@gmail.com>
2018-03-22 11:09:32 +0800

    init

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import index from '@/pages/index'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'index',
-      component: () => import('@/pages/index')
-    }
-  ]
-})
+    routes: [
+        {
+            path: '/',
+            name: 'index',
+            component: () => import('@/pages/index')
+        }
+    ]
+});
+
