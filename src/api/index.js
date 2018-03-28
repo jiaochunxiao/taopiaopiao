ddfbf8623f8347962a4b582d3596be3383895743
jiaochunxiao <jcx13627607951@gmail.com>
2018-03-28 21:35:45 +0800

    init

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,23 @@
+import Vue from 'vue';
 import axios from 'axios';
+import qs from 'qs';
+
+axios.default.timeout = 5000;
+// axios.defautl.baseURL = '';
+
+
+axios.interceptors.request.use(config => {
+    // todo
+    return config;
+}, error => {
+    // todo
+    return Promise.reject(error);
+});
+
+axios.interceptors.response(response => {
+    // todo
+    return response;
+}, error => {
+    // todo
+    return Promise.reject(error);
+});
